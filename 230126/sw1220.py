43bceeb11409b7e8bf41072537afa6abe7578f6c
Yechan Kang <ghn05188@naver.com>
2023-01-26 15:51:38 +0900

    commit

--- 230126/sw1220.py
+++ 230126/sw1220.py
@@ -11,19 +11,19 @@ for test_case in range(1, 11):
         temp = 0
         firstN = -1
         firstS = -1
-        for j in range(100):
-            if arr[j][i] == 1:
+        for j in range(100):    # 세로로 내려가면서 확인
+            if arr[j][i] == 1:  # 제일 처음 나오는 N의 세로 인덱스 저장
                 firstN = j
                 break
-        for j in range(99, -1, -1):
+        for j in range(99, -1, -1): # 제일 처음 나오는 S의 세로 인덱스 저장
             if arr[j][i] == 2:
                 firstS = j
                 break
-        if firstN == -1 or firstS == -1:
+        if firstN == -1 or firstS == -1:    # N 또는 S 둘 중 하나라도 없으면 다음 가로줄로 넘어감
             continue
-        elif firstN > firstS:
+        elif firstN > firstS:   # 처음 오는 N이 처음 오는 S보다 밑에 있으면 넘어감
             continue
-        for j in range(firstN, firstS + 1):
+        for j in range(firstN, firstS + 1): # 처음 N과 처음 S사이에 있는 모든 자성체 세기
             if arr[j][i] != 0:
                 temp += 1
         result += temp
